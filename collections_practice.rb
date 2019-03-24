ee77db3f224eb3480e37bebc7613c460efd70a98
Gregory Losco <gtlosco@gmail.com>
2019-03-24 22:12:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,8 +56,7 @@ end
 
 def add_s(word)
   new_words = []
-  word.each {|part| next if word = "feet"
-   new_words << "#{part}s" 
+  word.each {|part| new_words << "#{part}s" 
   }
   new_words
 end
